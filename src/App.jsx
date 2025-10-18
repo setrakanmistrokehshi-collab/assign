792a59b16d0c724a8af7ee9a2675d3f1a70d0e15
setrakanmistrokehshi <setrakanmistrokehshi@gmail.com>
2025-10-18 09:07:13 -0700

    assign

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './component/Nav'
+import Card from "./component/Card"
+import Container from '@mui/material/Container'
+
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Container/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Card.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import axios from "axios";
+import image from "../assets/image.jpg"
+
+// Expand button styling
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  marginLeft: "auto",
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function RecipeReviewCard() {
+  const [expanded, setExpanded] = useState(false);
+  const [data, setData] = useState([]);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setData(res.data.slice(0,9)); //Get first 9 items
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching data", err);
+      });
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "20px",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
+      {data.map((info) => (
+        <Card key={info.id} sx={{ maxWidth: 345 }}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                {info.title.charAt(0).toUpperCase()}
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={info.title}
+            subheader="JSONPlaceholder Data"
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image={image}
+            alt="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              {info.body}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>More Info:</Typography>
+              <Typography paragraph>
+                This is sample expandable content for post ID {info.id}.
+              </Typography>
+              <Typography paragraph>
+                The goal is to demonstrate expandable cards using React and
+                Material-UI.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      ))}
+    </div>
+  );
+}
